3aac89716e7153835e777a073e1b008516e209c8
Jadbinder <jadkang1@gmail.com>
2024-01-25 17:52:49 -0800

    Revert "Big"
    This reverts commit b418bcf3258c0fe308cc8445812a10f92dc6cddb.

--- Master branch/SmartShop.UI/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace SmartShop.UI.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
--- Master branch/SmartShop.UI/Models/SmartShopApiConfig.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SmartShop.UI.Models
-{
-    public class SmartShopApiConfig
-    {
-        public string ApiKey { get; init; }
-        public string ApiSecret { get; init; }
-        public string BaseUri { get; init; }
-    }
-}
